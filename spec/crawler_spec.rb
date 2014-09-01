3e8123ffdeb4e67f02ab1eff6d8040905b4d5148
Alexander Kahn <alexanderkahn@gmail.com>
2014-09-01 16:36:53 -0400

    Improve Crawler expectation names

--- spec/crawler_spec.rb
+++ spec/crawler_spec.rb
@@ -2,7 +2,7 @@ require 'helper'
 require 'spidey/crawler'
 
 describe Spidey::Crawler do
-  it "should find child pages" do
+  it "finds child pages" do
     results = Spidey::Crawler.new('http://localhost:8000', 100).crawl
     expect(results.keys).to eq(["http://localhost:8000/",
                                 "http://localhost:8000/sub_page.html",
@@ -10,14 +10,14 @@ describe Spidey::Crawler do
                                 "http://localhost:8000/sub_sub_page.html"])
   end
 
-  it "should limit depth of link traversal" do
+  it "limits depth of link traversal" do
     results = Spidey::Crawler.new('http://localhost:8000', 2).crawl
     expect(results.keys).to eq(["http://localhost:8000/",
                                 "http://localhost:8000/sub_page.html",
                                 "http://localhost:8000/promotion.html"])
   end
 
-  it "should contain links between pages" do
+  it "contains links between pages" do
     results = Spidey::Crawler.new('http://localhost:8000', 1).crawl
     expect(results.values.first.links).to eq(["/sub_page.html",
                                               "http://localhost:8000/promotion.html"])
